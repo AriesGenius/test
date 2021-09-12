f23a5c901f82bc30feda60a13f199be2ed067a48
AriesGenius <zjs1115170255@gmail.com>
2021-09-13 03:30:54 +0930

    test

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+import Amplify from 'aws-amplify';
+import awsconfig from './aws-exports';
+import {AmplifySignOut, withAuthenticator} from '@aws-amplify/ui-react'
+
+Amplify.configure(awsconfig)
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       <AmplifySignOut/>
+       <h2>Welcome to Nuber</h2>
+      </header>
+    </div>
+  );
+}
+
+export default withAuthenticator(App);
